49a4a75f7119883c8675037e02fc37ad2a2f003b
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona@gmail.com>
2023-08-02 17:25:56 +0300

    feat: add all fonctionality on react app without css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import TodoForm from "./components/TodoForm";
+// import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="tod-app">
-      <TodoForm />
+    <div className="todo-app">
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
   const [input, setInput] = useState("");
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
   const handleChange = (e) => {
     setInput(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // props.onSubmit({
-    //   id: Math.floor(Math.random() * 10000),
-    //   text: input,
-    // });
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
     setInput("");
   };
   return (
@@ -23,6 +28,7 @@ function TodoForm(props) {
         name="text"
         className="todo-input"
         onChange={handleChange}
+        ref={inputRef}
       />
       <button className="todo-button">Add todo</button>
     </form>
